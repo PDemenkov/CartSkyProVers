2d9b7f2693469dd8586765b96ad7b4dc626f76f7
PDemenkov <guapnik@gmail.com>
2022-11-17 23:01:31 +0300

    initial commit

---
+++ src/main/java/com/example/cartskyprovers/CartSkyProVersApplication.java
@@ -0,0 +1,13 @@
+package com.example.cartskyprovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartSkyProVersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartSkyProVersApplication.class, args);
+    }
+
+}
